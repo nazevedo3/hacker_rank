ffff640376a32bcc50d66243af4ddfd8a1f232b7
Nathan Azevedo <nathan@Nathans-MacBook-Pro.local>
2021-04-10 13:33:27 -0700

    fixed inspect limit

--- elixir/07_array_n_elements.ex
+++ elixir/07_array_n_elements.ex
@@ -1,12 +1,12 @@
-defmodule Solution do
+defmodule ArrayNElements do
 
   def main() do
     number = IO.read(:stdio, :line)
       |> String.to_integer
 
     list_of_elements = for nums <- 1..number, do: nums
-    IO.inspect list_of_elements
+    IO.inspect(list_of_elements, limit: :infinity)
   end
 end
 
-Solution.main
+ArrayNElements.main()
